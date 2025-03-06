6879d8204eec0790ed0d51dfef00ee168874d487
Ryan Melfi <acacia314@gmail.com>
2025-03-06 16:01:01 -0500

    Revert "asd"
    This reverts commit 4d032c266a563c7f5f0d0514ffc21929b76bc408.

--- .eleventy.js
+++ .eleventy.js
@@ -53,10 +53,6 @@ module.exports = function (eleventyConfig) {
     return content;
   });
 
-  eleventyConfig.addFilter("json", function(value) {
-    return JSON.stringify(value, null, 2);
-  })
-
   // Let Eleventy transform HTML files as nunjucks
   // So that we can use .html instead of .njk
   return {
